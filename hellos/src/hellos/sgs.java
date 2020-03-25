6c96f778c9c0237aee06c5f9e4c3168ad0d4723e
lyl1754 <1754666708@qq.com>
2020-03-25 18:34:37 +0800

    11

---
+++ hellos/src/hellos/sgs.java
@@ -0,0 +1,19 @@
+package hellos;
+/**@文件名: sgs.java
+ * @类功能说明: 
+ * @作者: liyu
+
+ * @Email: 1754666708@qq.com
+ * @日期: 2020年3月25日下午5:09:28
+ * @修改说明:<br> 
+ * <pre>
+ * 	 <li>作者: 李宇</li> 
+ * 	 <li>日期: 2020年3月25日下午5:09:28</li> 
+ *	 <li>内容: </li>
+ * </pre>
+ */
+public class sgs {
+public static void main(String[] args) {
+	System.out.println("hello");
+}
+}
